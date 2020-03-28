847757d01ae6380e9ac1dadb47448c7c47b77151
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 19:19:06 +0530

    override toString method and use StringBuilder to store the content

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -38,6 +38,20 @@ public class MyQueue<E> {
         return data;
     }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("[");
+        Node<E> temp = front;
+        for (int i = 0; i < size && temp != null; i++) {
+            E data = temp.getData();
+            sb.append(data);
+            sb.append((i < size - 1) ? "," : "");
+            temp = temp.getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
     private static class Node<E> {
         E data;
         Node<E> next;
