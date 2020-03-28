7c864e2ac6e256035f1525458b2e0e3bb7d56008
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 19:12:30 +0530

    create getter and setter method of the fields

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -20,5 +20,21 @@ public class MyQueue {
             this.data = data;
             this.next = next;
         }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
 }
