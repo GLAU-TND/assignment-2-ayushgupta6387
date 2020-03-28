f4ede1ff8ace3dddcad17bc81b0c7a625ca8ac23
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 14:20:31 +0530

    constructor to initialize the fields

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -10,4 +10,10 @@ public class TreeNode {
     private TreeNode left;
     private TreeNode right;
     private int data;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
