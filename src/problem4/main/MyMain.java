e2cb41b20d4203167381a21e0373799b08deb47a
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 20:09:54 +0530

    create method for preOrder Traversing

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -6,6 +6,10 @@
  */
 package problem4.main;
 
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
 import java.util.Scanner;
 
 // executable class
@@ -16,6 +20,32 @@ public class MyMain {
         System.out.println("Enter the elements which are added in binary search tree");
         String s = scanner.nextLine();
         String[] ar = s.split(" ");
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        for (String s1 : ar) {
+            tree.insert(Integer.parseInt(s1));
+            MyQueue<Integer> myQueue = new MyQueue<>();
+            System.out.println("Nodes After traversing in Pre Order :");
+            MyQueue<Integer> myQueue1 = preOrderTraverse(tree.getRoot(), myQueue);
+            System.out.println();
+            System.out.println("Queue of Pre Order Successor :");
+            MyQueue<Integer> myQueue2 = getQueuePreOrderSuccessor(myQueue1);
+            System.out.println(myQueue2);
+        }
+    }
 
+    public static MyQueue preOrderTraverse(TreeNode currentNode, MyQueue<Integer> myQueue) {
+        if (currentNode != null) {
+            myQueue.enqueue(currentNode.getData());
+            System.out.print(currentNode.getData() + " ");
+            preOrderTraverse(currentNode.getLeft(), myQueue);
+            preOrderTraverse(currentNode.getRight(), myQueue);
+        }
+        return myQueue;
+    }
+
+    public static MyQueue getQueuePreOrderSuccessor(MyQueue myQueue) {
+        myQueue.dequeue();
+        return myQueue;
     }
 }
+
