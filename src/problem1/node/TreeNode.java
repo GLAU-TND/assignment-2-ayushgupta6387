4f120e2c6c93c56305adf6a211d4b82b8cd96487
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 14:36:40 +0530

    create getter and setter method for data field

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -32,4 +32,12 @@ public class TreeNode {
     public void setRight(TreeNode right) {
         this.right = right;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }
