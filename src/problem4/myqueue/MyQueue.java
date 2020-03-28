c3656c688d56ae82c2212865db225f33523ba0d8
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 19:11:33 +0530

    create constructor for all fields

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -11,5 +11,14 @@ public class MyQueue {
     private static class Node<E> {
         E data;
         Node<E> next;
+
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
