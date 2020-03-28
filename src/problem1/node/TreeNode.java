259d93d8152bbfb7298827015382f833e39a00b3
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 14:22:16 +0530

    create getter and setter method for the left and right fields

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -16,4 +16,20 @@ public class TreeNode {
         this.left = null;
         this.right = null;
     }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
