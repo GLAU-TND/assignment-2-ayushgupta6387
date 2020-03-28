334a790333c729b32b1b40d88616c9c8bd52814b
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 16:44:56 +0530

    create constructor for the fields

--- src/problem3/details/Student.java
+++ src/problem3/details/Student.java
@@ -4,4 +4,13 @@ public class Student {
     private int rollNumber;
     private String name;
 
+    public Student(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
 }
