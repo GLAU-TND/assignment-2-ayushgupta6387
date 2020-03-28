068753471b0d08594bb85f36d211044cf7ea4674
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 16:00:18 +0530

    create object of MyBinarySearchTree and add elements

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,33 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(50);
+        tree.insert(25);
+        tree.insert(75);
+        tree.insert(15);
+        tree.insert(35);
+        tree.insert(65);
+        tree.insert(80);
+        tree.insert(7);
+        tree.insert(32);
+        tree.insert(31);
+        tree.insert(33);
+        tree.insert(67);
+        tree.insert(90);
+        tree.insert(82);
+        tree.insert(93);
+        tree.inOrderTraverse(tree.getRoot());
+        System.out.println();
+        tree.preOrderTraverse(tree.getRoot());
+        System.out.println();
+        tree.postOrderTraverse(tree.getRoot());
+        System.out.println();
+        tree.printLeftChild(tree.getRoot());
+    }
 }
--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,34 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+
 // executable class
+
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        tree.insert(50);
+        tree.insert(25);
+        tree.insert(75);
+        tree.insert(15);
+        tree.insert(35);
+        tree.insert(65);
+        tree.insert(80);
+        tree.insert(7);
+        tree.insert(32);
+        tree.insert(31);
+        tree.insert(33);
+        tree.insert(67);
+        tree.insert(90);
+        tree.insert(82);
+        tree.insert(93);
+        System.out.println("Traverse the tree in PreOrder :");
+
+    }
+
 }
