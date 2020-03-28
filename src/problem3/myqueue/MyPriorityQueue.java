39f303ffdabc012f3fd4b22f791a3f71dd3110e3
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-28 16:42:40 +0530

    override to string method and use stringbuilder to store the values

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -64,4 +64,19 @@ public class MyPriorityQueue<E> {
         return data;
     }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("[");
+        Node<E> temp = front;
+        for (int i = 0; i < size && temp != null; i++) {
+            E data = temp.getData();
+            sb.append(data);
+            sb.append((i < size - 1) ? ",\n" : "");
+            temp = temp.getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
+
+
