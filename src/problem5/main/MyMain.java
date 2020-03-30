d33ce6c6bc943a084de389cf877643d14a79670d
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-30 21:56:00 +0530

    import scanner class and start taking input

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,18 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        MyCircularQueue circularQueue = new MyCircularQueue();
+
+        System.out.println("Enter number of records you wants to enter");
+        int numberOfRecords = scan.nextInt();
+    }
 }
