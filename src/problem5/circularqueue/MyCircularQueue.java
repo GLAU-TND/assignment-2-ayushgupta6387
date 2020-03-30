42bbd53aec9b9d8531455025b6d6802c19f18d15
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-30 21:28:07 +0530

    create required fields for the queue

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,12 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem3.node.Node;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private Node front;
+    private Node rear;
+
 }
