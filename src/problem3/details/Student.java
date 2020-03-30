a7a81191eb23ff743ff81287d447afc3e18d6a1a
ayushgupta6387 <ayush.gupta1_cs18@gla.ac.in>
2020-03-30 22:53:40 +0530

    this class completed

--- src/problem3/details/Student.java
+++ src/problem3/details/Student.java
@@ -6,10 +6,6 @@ public class Student {
     private int rollNumber;
     private String name;
 
-    public Student(int rollNumber) {
-        this.rollNumber = rollNumber;
-    }
-
     public Student(int rollNumber, String name) {
         this.rollNumber = rollNumber;
         this.name = name;
